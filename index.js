510549af4e557daaa8eca32c53723904145a5e58
Do Cong Vinh <vinhdocong109@gmail.com>
2024-08-10 10:55:51 +0700

    commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import openai from "./openai";
+
+// const chatCompletion = await openai.chat.completions.create({
+//   messages: [{ role: "user", content: "write poem about today" }],
+//   model: "gpt-3.5-turbo",
+// });
+
+// console.log(chatCompletion);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+// const openai = new OpenAI({
+//   apiKey:
+//     "sk-utiTbeawZznOLbDiN82XXwmsHLi9zNCt4mbaoP0Hg5T3BlbkFJuo6AlSuxDFxsdkCty7gCPv-8jUu80tRFQ0CwP2SXEA",
+// });
